a2702ba1be4ce8a9280b64d9eca0e6c499c8e7c7
peikovakate <peikovakate@gmail.com>
2017-03-05 23:07:32 +0200

    player extended to Human and AI

---
+++ CheckersGame/Source/Human.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersGame.Source
+{
+    class Human : Player
+    {
+        public Human(CheckerColor color) : base(color)
+        {
+        }
+    }
+}
